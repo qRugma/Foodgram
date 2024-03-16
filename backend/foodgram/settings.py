042926ef9920258ee75158f57d0fc99eb21d8ade
qRugma <strugov.poma@yandex.ru>
2024-03-16 17:23:15 +0600

    all

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -146,6 +146,5 @@ STATIC_ROOT = '/collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-if not DEBUG:
-    MEDIA_ROOT = ''
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
